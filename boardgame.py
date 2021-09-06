759a497834f810a73922fe1bc0109df8b76d11d0
Jonatas Amaral <jonataspadtab@gmail.com>
2021-09-06 03:37:14 -0300

    minimizing code for checker pattern

--- boardgame.py
+++ boardgame.py
@@ -6,12 +6,11 @@ cell_white = '🔲'
 
 # print out the board
 for l in range(lines):
-	if l%2:
-		for c in range(cols):
-			if c%2: print(cell, end=" ")
-			else: print(cell_white, end=" ")
-	else:
-		for c in range(cols):
-			if c%2: print(cell_white, end=" ")
-			else: print(cell, end=" ")
+	for c in range(cols):
+		even = (l*lines+c)%2
+		odd = 1-even
+		print(
+			cell * even +
+			cell_white * odd
+		, end=" ")
 	print()
