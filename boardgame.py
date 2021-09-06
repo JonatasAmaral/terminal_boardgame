ea02c6189d1f0573cb7c3db8e826bee2acafe3a8
Jonatas Amaral <jonataspadtab@gmail.com>
2021-09-06 02:55:23 -0300

    checkerboard board pattern insteat of black tiles

--- boardgame.py
+++ boardgame.py
@@ -2,9 +2,16 @@
 cols = 5
 lines = 5
 cell = '🔳' # ⬛
+cell_white = '🔲'
 
 # print out the board
 for l in range(lines):
-	for c in range(cols):
-		print( cell, end=" ")
+	if l%2:
+		for c in range(cols):
+			if c%2: print(cell, end=" ")
+			else: print(cell_white, end=" ")
+	else:
+		for c in range(cols):
+			if c%2: print(cell_white, end=" ")
+			else: print(cell, end=" ")
 	print()
