3d0a1c7e43de61f2dfa830852f7e7b31232d3dfe
Jonatas Amaral <jonataspadtab@gmail.com>
2021-09-06 02:32:29 -0300

    create board side coords numbering

--- boardgame.py
+++ boardgame.py
@@ -4,7 +4,26 @@ lines = 5
 cell = '🔳' # ⬛
 
 # print out the board
-for l in range(lines):
-	for c in range(cols):
-		print( cell, end=" ")
+for l in range(-1,lines):
+	# if on line of columns' labels
+	if l == -1:
+		for c in range(-1, cols):
+			# if on column of lines' labels
+			if c == -1:
+				# print empty space
+				print('  ', end=" ")
+			else:
+				# print columns number
+				print(c, end="  ")
+
+	else:
+		for c in range(-1, cols):
+			# if on column of lines' labels
+			if c == -1:
+				# print line number
+				print(l, end=" ")
+			else:
+				# print cell
+				print( cell, end=" ")
+
 	print()
