b1b86721a496666133599e33edd69d07f3c963d0
Jonatas Amaral <jonataspadtab@gmail.com>
2021-09-11 00:06:14 -0300

    move char info to variables

--- boardgame.py
+++ boardgame.py
@@ -3,10 +3,15 @@ cols = 5
 lines = 5
 cell = "🔳" # ⬛
 
+character_image = "😀"
+character_position_col = 2
+character_position_line = 4
+
 # print out the board
 for l in range(lines):
 	for c in range(cols):
 		# place a character in the board
-		if l == 4 and c == 2: print( "😀", end=" ")
+		if l == character_position_line and c==character_position_col:
+			print( character_image , end=" ")
 		else: print( cell, end=" ")
 	print()
