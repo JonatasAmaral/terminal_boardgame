00e188b22097aef12ea67f1d3ce1ba5d836a0858
Jonatas Amaral <jonataspadtab@gmail.com>
2021-09-11 00:23:50 -0300

    condensate char position to a vector/array/list

--- boardgame.py
+++ boardgame.py
@@ -4,14 +4,13 @@ lines = 5
 cell = "🔳" # ⬛
 
 character_image = "😀"
-character_position_col = 2
-character_position_line = 4
+character_position = [2,4] # [x,y]
 
 # print out the board
 for l in range(lines):
 	for c in range(cols):
 		# place a character in the board
-		if l == character_position_line and c==character_position_col:
+		if l==character_position[1] and c==character_position[0]:
 			print( character_image , end=" ")
 		else: print( cell, end=" ")
 	print()
