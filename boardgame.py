80b441be6e7fc119de85a21ab59771e617d8bf24
Jonatas Amaral <jonataspadtab@gmail.com>
2021-09-11 00:31:35 -0300

    condensate char infos into a dict/object/map

--- boardgame.py
+++ boardgame.py
@@ -1,16 +1,19 @@
 # definitions
 cols = 5
 lines = 5
-cell = "🔳" # ⬛
+cell = "🔳"  # ⬛
 
-character_image = "😀"
-character_position = [2,4] # [x,y]
+char = {
+	"image": "😀",
+	"pos": [2,4]  # [x,y]
+}
 
 # print out the board
 for l in range(lines):
-	for c in range(cols):
-		# place a character in the board
-		if l==character_position[1] and c==character_position[0]:
-			print( character_image , end=" ")
-		else: print( cell, end=" ")
-	print()
+    for c in range(cols):
+        # place a character in the board
+        if c == char["pos"][0] and l == char["pos"][1]:
+            print(char["image"], end=" ")
+        else:
+            print(cell, end=" ")
+    print()
